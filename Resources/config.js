a6c8f0c0ab7f68a897ac9f3524ce65211e1d4f86
motoy3d <motoy3d@gmail.com>
2014-12-15 04:25:07 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'vortis'
+    ,hashtag: 'vortis'
+    ,teamName: '徳島'
+    ,appName: 'スマートJ for 徳島ヴォルティス'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id929041403?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.vortis'
+    ,color: '#0000af'
+    ,tintColor: 'white'
+    ,dbName: 'smartj.vortis'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=vortis&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=vortis"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 254873
+    ,nendApiKeyIPhoneIcon: '36555005f322c654683684d3da4b7e8a7e837c29'
+    ,nendSpotIdIPhoneBanner: 254872
+    ,nendApiKeyIPhoneBanner: 'c56e1c56c1d92641f9751da18f15bc3017ce7298'
+    //nend(Android)
+    ,nendSpotIdAndroid: 254871
+    ,nendApiKeyAndroid: 'a716ad483a8cc7dd08ad148a9c06521f7096f763'
+};
